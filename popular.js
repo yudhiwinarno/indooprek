a2825db7e6b60e25902957e8a81d16c11b665978
yudhiwinarno <yudhiwinarno82@gmail.com>
2017-11-15 21:33:02 +0700

    Update popular.js

--- popular.js
+++ popular.js
@@ -25,3 +25,4 @@ if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
+protected_links = "youtube.com,facebook.com,google.com";
